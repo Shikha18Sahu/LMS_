052d7799ec67d1feafa38b0eea8528d26a318f65
Shikha18Sahu <sahushikhauptu@gmail.com>
2025-07-02 13:38:04 +0530

    fix: correct typo in User model method call in stripeWebhooks function

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -88,7 +88,7 @@ export const stripeWebhooks = async(request, response)=>{
       const { purchaseId } = session.data[0].metadata;
 
       const purchaseData = await Purchase.findById(purchaseId)
-      const userData = await User.findyById(purchaseData.userId)
+      const userData = await User.findById(purchaseData.userId)
       const courseData = await Course.findById(purchaseData.courseId.toString())
 
       courseData.enrolledStudents.push(userData)
