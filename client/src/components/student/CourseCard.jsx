ac00c2490c30e3622b1543a2efcc2c91546d8e38
Shikha18Sahu <sahushikhauptu@gmail.com>
2025-07-11 15:35:18 +0530

    final complete

--- client/src/components/student/CourseCard.jsx
+++ client/src/components/student/CourseCard.jsx
@@ -14,7 +14,7 @@ const CourseCard = ({course}) => {
         <h3 className='text-base font-semibold'>{course.courseTitle}</h3>
         {/* <p className='text-gray-500'>{course.educator || "ShikhaSahu"}</p> */}
        <p className='text-gray-500'>
-  {course.educator && typeof course.educator === 'object'
+  {course.educator && typeof course.educator === 'object' && course.educator.name
      ? course.educator.name
      : (course.educator || "ShikhaSahu")}
 </p>
