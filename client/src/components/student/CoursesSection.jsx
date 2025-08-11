81b440553fb869f04f1633fd8e422fe0740fcd12
Shikha18Sahu <sahushikhauptu@gmail.com>
2025-08-11 22:52:42 +0530

    fix: adjust layout of CoursesSection component for better alignment

--- client/src/components/student/CoursesSection.jsx
+++ client/src/components/student/CoursesSection.jsx
@@ -23,8 +23,9 @@ const CoursesSection = () => {
 ))}
 
       </div>
-
+      <div className= "flex justify-center mt-8">
        <Link to={'/course-list'} onClick={()=> scrollTo(0,0)} className='text-gray-500 border border-gray-500/30 px-10 py-3 rounded'>Show all courses</Link>
+    </div>
     </div>
   )
 }
